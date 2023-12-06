4ba3021cd6107af6facf86e88e5a59e5be13540c
nir zamir <nirozamir@gmail.com>
2023-12-06 19:24:24 +0200

    Add project files.

---
+++ Peer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Udp
+{
+    public class Peer
+    {
+        private IPEndPoint _endPoint;
+        private UdpClient _client;
+        private const int _port = 8888;
+        public Peer(IPAddress peerIp, int port)
+        {
+            _client = new UdpClient(_port);
+            _endPoint = new IPEndPoint(peerIp, port);
+        }
+        public void send(byte[] bytes)
+        {
+            _client.Send(bytes,_endPoint);
+        }
+        public Task<UdpReceiveResult> recieve()
+        {
+            return _client.ReceiveAsync();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Udp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
